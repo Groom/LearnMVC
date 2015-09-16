d3b078d5650c1bec9c32aab54974d25f40ec7908
Gorm Kjeldsen <gk@gormkjeldsen.dk>
2015-09-16 16:04:40 +0200

    Day 6, Lab 31
    Haven't done the steps in "Talk on lab 31", though. Should probably do
    that next.

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,10 +1,4 @@
-﻿using LearnMVC.DataAccessLayer;
-using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -18,8 +12,6 @@ namespace LearnMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SalesERPDAL>());
         }
     }
 }
