a667bb1250b8f3ede3adf8cc1c5cef777d03670a
Gorm Kjeldsen <gk@gormkjeldsen.dk>
2015-09-16 14:02:01 +0200

    Day 6, Lab 27

---
+++ Controllers/BulkUploadController.cs
@@ -0,0 +1,52 @@
+﻿using LearnMVC.Filters;
+using LearnMVC.Models;
+using LearnMVC.ViewModels;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+
+namespace LearnMVC.Controllers
+{
+    public class BulkUploadController : Controller
+    {
+        //
+        // GET: /BulkUpload/
+        [HeaderFooterFilter]
+        [AdminFilter]
+        public ActionResult Index()
+        {
+            return View(new FileUploadViewModel());
+        }
+
+        [AdminFilter]
+        public ActionResult Upload(FileUploadViewModel model)
+        {
+            List<Employee> employees = GetEmployees(model);
+            EmployeeBusinessLayer bal = new EmployeeBusinessLayer();
+            bal.UploadEmployees(employees);
+
+            return RedirectToAction("Index", "Employee");
+        }
+
+        private List<Employee> GetEmployees(FileUploadViewModel model)
+        {
+            List<Employee> employees = new List<Employee>();
+            StreamReader csvReader = new StreamReader(model.fileUpload.InputStream);
+            csvReader.ReadLine(); // Assuming first line is header
+            while (!csvReader.EndOfStream)
+            {
+                var line = csvReader.ReadLine();
+                var values = line.Split(',');
+
+                Employee e = new Employee();
+                e.FirstName = values[0];
+                e.LastName = values[1];
+                e.Salary = int.Parse(values[2]);
+
+                employees.Add(e);
+            }
+
+            return employees;
+        }
+	}
+}
